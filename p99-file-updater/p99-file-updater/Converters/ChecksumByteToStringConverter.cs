7818a740f36c8d202e269963c37d2dd6e6206c92
John H. Freeman <egot@legacyofthefallen.net>
2026-01-04 16:15:42 -0600

    updated byte converter and name to indicate usage

--- p99-file-updater/p99-file-updater/Converters/ChecksumByteToStringConverter.cs
+++ p99-file-updater/p99-file-updater/Converters/ChecksumByteToStringConverter.cs
@@ -4,32 +4,41 @@ using Windows.UI.Xaml.Data;
 
 namespace p99FileUpdater.Converters
 {
-    internal class ChecksumByteToStringConverter : IValueConverter
+    internal class SHA256ChecksumByteToStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             if(value == null)
                 return String.Empty;
             StringBuilder sb = new StringBuilder();
-            byte[] checksumToConvert = (byte[])value;
-            foreach (byte b in checksumToConvert)
+            foreach (byte b in (byte[])value)
                 sb.Append(b.ToString("x2"));
             return sb.ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
-            if ((string)value == String.Empty)
-                return null;
-            char[] str = ((String)value).ToCharArray();
-            byte[] checksum = new byte[32];
-            for (int i = 0; i < 32; i++)
-        {
-                String s = String.Concat(str[i*2], str[(i*2) + 1]);
-                byte b = System.Convert.ToByte(s, 16);
-                checksum[i] = b;
-        }
-            return checksum;
+            const int sha256strlength = 32;
+            string strValue = (string)value;
+            char[] str = strValue.ToCharArray();
+            try
+            {
+                if (str.Length != sha256strlength)
+                    throw new ArgumentException($"str value cast is not of length {sha256strlength} but {str.Length}");
+                byte[] checksum = new byte[sha256strlength];
+                for (int i = 0; i < sha256strlength; i++)
+                {
+                    byte b = System.Convert.ToByte(String.Concat(str[i * 2], str[(i * 2) + 1]), sha256strlength / 2);
+                    checksum[i] = b;
+                }
+                return checksum;
+            }
+            catch (Exception ex)
+            {
+                {
+                    return null;
+                }
+            }
         }
     }
 }
